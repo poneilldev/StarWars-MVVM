04351bd23305a76895d60289dda1ecf74e223ebb
Paul O'Neill <paul_oneill@apple.com>
2022-03-15 11:09:49 -0500

    Added examples of converting non-async await methods

--- StarWars/StarWars/Networking/NetworkingManager.swift
+++ StarWars/StarWars/Networking/NetworkingManager.swift
@@ -10,6 +10,7 @@ import UIKit
 
 enum NetworkManagerError: Error {
     case badHTTPResponse
+    case noData
     case badImageData
     case decodingError(Error)
 }
@@ -48,6 +49,52 @@ struct NetworkingManager {
         cache.cacheImage(image, for: url)
         
         return image
+    }
+    
+    // The following two methods are examples of turning a non-async method with a closure into an async method
+    
+    @available(*, renamed: "oldLoadResource(type:with:)")
+    static func oldLoadResource<Element: Decodable>(type: Element.Type, with url: URL, completion: @escaping (_ result: Result<Element, Error>) -> Void) {
+        Task {
+            do {
+                let result: Element = try await oldLoadResource(type: type, with: url)
+                completion(.success(result))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    
+    static func oldLoadResource<Element: Decodable>(type: Element.Type, with url: URL) async throws -> Element {
+        return try await withCheckedThrowingContinuation { continuation in
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                guard error == nil else {
+//                    continuation.resume(with: .failure(error!))
+                    continuation.resume(throwing: error!)
+                    return
+                }
+                
+                guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                    continuation.resume(with: .failure(NetworkManagerError.badHTTPResponse))
+                    return
+                }
+                
+                guard let data1 = data else {
+                    continuation.resume(with: .failure(NetworkManagerError.noData))
+                    return
+                }
+                
+                do {
+                    let results = try JSONDecoder().decode(Element.self, from: data1)
+                    continuation.resume(with: .success(results))
+                } catch let error {
+                    continuation.resume(with: .failure(error))
+                }
+            }
+            
+            task.resume()
+        }
     }
 }
 
